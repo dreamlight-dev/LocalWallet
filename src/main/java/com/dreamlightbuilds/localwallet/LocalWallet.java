646aaf0a5d928752ea8043ebffded5a6471c244f
mdaffa48 <work@muhammaddaffa.com>
2022-05-24 18:29:38 +0700

    Initial commit

---
+++ src/main/java/com/dreamlightbuilds/localwallet/LocalWallet.java
@@ -0,0 +1,18 @@
+package com.dreamlightbuilds.localwallet;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LocalWallet extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+}
